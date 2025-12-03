077f4c06fb9db9c51b8e100832ec3d7ce94369d8
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-04 02:22:08 +0500

    initial commit

--- 3-Loops/Examples.java
+++ 3-Loops/Examples.java
@@ -1,46 +1,46 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class Examples {
     
     public static void main(String[] args) {
         
-        // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        // System.out.print("Enter Number: ");
-        // int n = sc.nextInt();
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
 
-        // // Reverse of a number.
-        // int count = 0;
+        // Reverse of a number.
+        int count = 0;
 
-        // while (n>0) {
+        while (n>0) {
             
-        //     count = n % 10;
-        //     n = n/10;
-        //     System.out.print(count);
+            count = n % 10;
+            n = n/10;
+            System.out.print(count);
             
-        // }
+        }
        
         
 
-        // int rev = 0;
+        int rev = 0;
         
-        // while (n>0) {
-        //     int count = n%10;
-        //     rev = (rev * 10) + count;
-        //     n=n/10;
-        // }
-        // System.out.println("Reverse: " + rev);
+        while (n>0) {
+            int countCheck = n%10;
+            rev = (rev * 10) + countCheck;
+            n=n/10;
+        }
+        System.out.println("Reverse: " + rev);
 
 
-        int n = 5;
+        int num = 5;
         int sum = 0;
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= num; i++) {
             sum += i*i;
             
         }
         System.out.println(sum);
 
-        
+        sc.close();
     }
 }
