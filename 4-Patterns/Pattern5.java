859dbef67fa088bf36291aed930f28ed37e6fca3
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-05 05:14:27 +0500

    first commit

--- 4-Patterns/Pattern5.java
+++ 4-Patterns/Pattern5.java
@@ -2,10 +2,10 @@ public class Pattern5 {
     
     public static void main(String[] args) {
         
-        // int n = 3;
+        // int num = 3;
 
-        // for (int i = 1; i <= n; i++) {
-        //     for (int j = 1; j <= n-i+1; j++) {
+        // for (int i = 1; i <= num; i++) {
+        //     for (int j = 1; j <= num-i+1; j++) {
         //         System.out.print("*");
         //     }
         //     System.out.println();
