7ffe4cb1efc707a8e8450f75839502339d7adc27
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-09 23:46:03 +0500

    first commit

--- 6-Arrays/LinearSearch.java
+++ 6-Arrays/LinearSearch.java
@@ -1,27 +1,14 @@
 public class LinearSearch {
     
-    // public static int linear(int numbers[], int key) {
+    public static int linear(int numbers[], int key) {
         
-    //     for (int i = 0; i < numbers.length; i++) {
-    //         if (key == numbers[i]) {
-    //             return i;
-    //         }
-    //     }
-    //     return -1;
-    // }
-    // public static void main(String[] args) {
-    //     int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
-    //     int key = 18;
-
-    //     int index = linear(numbers, key);
-
-    //     if (index == -1) {
-    //         System.out.println("Key not found");
-    //     }
-    //     else{
-    //         System.out.println("Key is found at index: " + index);
-    //     }    
-    // }
+        for (int i = 0; i < numbers.length; i++) {
+            if (key == numbers[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 
     public static int items(String menu_cart[], String key ) {
@@ -35,10 +22,25 @@ public class LinearSearch {
     }
     
     public static void main(String[] args) {
-        String menu_cart[] = {"Pizza", "Burger", "Shawarma", "Drinks", "Sweets"};
-        String key = "kebab";
+        // Check for numbers..
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 18;
 
-        int index = items(menu_cart, key);
+        int idx = linear(numbers, key);
+
+        if (idx == -1) {
+            System.out.println("Key not found");
+        }
+        else{
+            System.out.println("Key is found at index: " + idx);
+        }    
+
+
+        // Check for items..
+        String menu_cart[] = {"Pizza", "Burger", "Shawarma", "Drinks", "Sweets"};
+        String target = "kebab";
+
+        int index = items(menu_cart, target);
 
         if (index == -1) {
             System.out.println("key NOT found");
