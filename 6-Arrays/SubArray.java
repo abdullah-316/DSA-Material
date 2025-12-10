21c272a98eb30c0649ccec90c0c076978e69eeae
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-10 06:51:41 +0500

    first commit

---
+++ 6-Arrays/SubArray.java
@@ -0,0 +1,24 @@
+public class SubArray {
+
+    public static void sub_array(int num[]) {
+
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i; j < num.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(num[k] + " ");
+
+                }
+                System.out.println();
+
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int num[] = { 2, 4, 6, 8, 10 };
+        sub_array(num);
+    }
+}
