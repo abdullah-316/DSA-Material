a187d55ecd2b9461cedb1b388f724024958c8bef
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-18 21:11:48 +0500

    first commit

--- 8-Two Pointer Pattern/TwoSum.java
+++ 8-Two Pointer Pattern/TwoSum.java
@@ -32,7 +32,7 @@ public class TwoSum {
 
     public static void main(String[] args) {
         int nums[] = { 2, 7, 11, 15 };
-        int target = 9;
+        int target = 18;
         int index[] = twoSum(nums, target);
 
         System.out.println("[" + index[0] + ", " + index[1] + "]");
