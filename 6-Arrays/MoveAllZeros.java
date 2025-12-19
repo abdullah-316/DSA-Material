474f9fd080d9a827515b972b65be9a457cd1192e
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-19 12:35:30 +0500

    first commit

--- 6-Arrays/MoveAllZeros.java
+++ 6-Arrays/MoveAllZeros.java
@@ -4,18 +4,33 @@ public class MoveAllZeros {
     
     public static void zero(int arr[]) {
         
-        int pos = 0;
+        // int pos = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0) {
-                arr[pos] = arr[i];
-                System.out.print(arr[pos]+" ");
-                pos++;
-            }    
+        // for (int i = 0; i < arr.length; i++) {
+        //     if (arr[i] != 0) {
+        //         arr[pos] = arr[i];
+        //         System.out.print(arr[pos]+" ");
+        //         pos++;
+        //     }    
+        // }
+        // for (int i = pos; i < arr.length; i++) {
+        //     arr[pos] = 0;
+        //     System.out.print(arr[pos]+ " ");
+        // }
+
+        // Optimal
+        int slow = 0;
+
+        for (int fast = 0; fast < arr.length; fast++) {
+            if (arr[fast] != 0 ) {
+                int temp = arr[slow];
+                arr[slow] = arr[fast];
+                arr[fast] = temp;
+                slow++;            }
         }
-        for (int i = pos; i < arr.length; i++) {
-            arr[pos] = 0;
-            System.out.print(arr[pos]+ " ");
+
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] +" ");
         }
         
     }
