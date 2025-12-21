b9cf30e00611c3d06f8cbe32851c0857be737f86
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-21 11:34:44 +0500

    first commit

---
+++ 7-Sorting/Builtin.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+public class Builtin {
+    
+    public static void printArr(Integer arr[]) {
+
+        for(Integer i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        
+        Integer arr[] = {5, 4, 1, 3, 2};
+
+        // Arrays.sort(arr);
+        // Arrays.sort(arr, 0 , 3);
+
+        // Reverse
+        Arrays.sort(arr, Collections.reverseOrder());
+        printArr(arr);
+    }
+}
