e90ebe5f20a9b31606a8f03b973ccdd307d4bf65
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-21 11:47:43 +0500

    first commit

--- 8-Two Pointer Pattern/MergeAndSorted.java
+++ 8-Two Pointer Pattern/MergeAndSorted.java
@@ -6,11 +6,14 @@ public class MergeAndSorted {
         int newArr[] = new int[n + m];
         int count = 0;
         while (i < n || j < m) {
+
+            // Corner case to check if my i value is equal to the length
             if (i == n) {
                 newArr[count] = arr2[j];
                 count++;
                 j++;
 
+            // Corner case to check if my j value is equal to the length
             } else if (j == m) {
                 newArr[count] = arr2[i];
                 count++;
@@ -33,6 +36,7 @@ public class MergeAndSorted {
 
     }
 
+    // function for printing values
     public static void printArr(int arr[]) {
         for (int i : arr) {
             System.out.print(i+" ");
@@ -43,8 +47,8 @@ public class MergeAndSorted {
     public static void main(String[] args) {
         int arr1[] = { 1, 3, 5 };
         int arr2[] = { 2, 4, 6 };
+        
         int merged[]= mergeSorted(arr1, arr2);
-
         printArr(merged);
     }
 }
