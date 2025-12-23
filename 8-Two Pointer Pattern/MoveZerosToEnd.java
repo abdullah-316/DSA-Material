8e01717858bc06d40a3e2a751818efa4ced71198
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-23 09:35:45 +0500

    first commit

---
+++ 8-Two Pointer Pattern/MoveZerosToEnd.java
@@ -0,0 +1,27 @@
+public class MoveZerosToEnd {
+    
+     public static void moveZeros(int nums[]) {
+        int n = nums.length;
+        int i = 0, j = 0;
+
+        while (j<n) {
+            if (nums[j] != 0 ) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+            j++;
+        }
+
+        for (int k = 0; k < nums.length; k++) {
+            System.out.print(nums[k] +" ");
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        int nums[] = {0,1,0,3,12};
+        moveZeros(nums);
+    }
+}
