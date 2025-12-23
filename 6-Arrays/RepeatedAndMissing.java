3f149548636fd8d8e86958c767aa0e63f7b3f155
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-23 09:23:52 +0500

    first commit

--- 6-Arrays/RepeatedAndMissing.java
+++ 6-Arrays/RepeatedAndMissing.java
@@ -1,27 +1,28 @@
 public class RepeatedAndMissing {
-    
-    public static int[] check(int arr[], int n ) {
-        
-        int duplicate = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    duplicate = arr[i];
-                }
-            }
-        }
 
-        int exp_output = n*((n+1))/2;
-        int sum = 0;
-        int totalFind = 0;
-        for (int i = 0; i < n-1; i++) {
-            sum += arr[i];
-            totalFind = (exp_output-duplicate) - sum;
+    public static int[] check(int arr[], int n) {
+
+        boolean[] check = new boolean[n];
+        int duplicate = 0, sum = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            int num = arr[i];
+
+            if (check[num]) {
+                duplicate = num;
+            }
+
+            check[num] = true;
+            sum += num;
         }
-        return new int[]{duplicate,totalFind};
+        int expectedSum = n * (n + 1) / 2;
+        int missing = (expectedSum - sum) - duplicate;
+
+        return new int[] { duplicate, missing };
     }
+
     public static void main(String[] args) {
-        int num[] = {3,1,2,5,3};
+        int num[] = { 3, 1, 2, 5, 3 };
         int n = 6;
 
         int index[] = check(num, n);
