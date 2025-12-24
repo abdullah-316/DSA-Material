b7b3c06a7a72f5b894412e6691e131938ed0ef51
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-24 09:08:49 +0500

    first commit

---
+++ 8-Two Pointer Pattern/TripletSmallerSum.java
@@ -0,0 +1,32 @@
+public class TripletSmallerSum {
+    
+    public static int smallerTriplet(int nums[], int target) {
+        int n = nums.length;
+        int ans = 0;
+
+        for (int i = 0; i < n-2; i++) {
+            
+            int left = i+1;
+            int right = n-1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum >= target) {
+                    right--;
+                }
+                else{
+                    ans = ans + (right-left);
+                    left++;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int nums[] = {-2,0,1,3};
+        // int nums[] = {5, 1, 3, 4, 7};
+        int target = 2;
+        System.out.println(smallerTriplet(nums, target));
+    }
+}
