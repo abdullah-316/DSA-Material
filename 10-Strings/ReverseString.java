c37349c8f0092a0a502b8bceb7c275715eab1f10
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-30 16:25:59 +0500

    first commit

---
+++ 10-Strings/ReverseString.java
@@ -0,0 +1,23 @@
+public class ReverseString {
+    
+    public static String reverse(String str) {
+      
+        StringBuilder sb = new StringBuilder(str);
+
+        int start = 0, end = sb.length()-1;
+        while (start < end) {
+            
+            char temp = sb.charAt(start);
+            sb.setCharAt(start, sb.charAt(end));
+            sb.setCharAt(end, temp);
+            start++;
+            end--;
+        }
+        
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String str = "OTC";
+        System.out.println(reverse(str));
+    }
+}
