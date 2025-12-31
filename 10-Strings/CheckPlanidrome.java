0e1544e004abade6935d53ca1e2cc454607af96a
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-31 22:05:12 +0500

    first commit

--- 10-Strings/CheckPlanidrome.java
+++ 10-Strings/CheckPlanidrome.java
@@ -1,18 +1,8 @@
 public class CheckPlanidrome {
     
     public static Boolean palindrome(String str) {
-        int n = str.length();
 
-        for (int i = 0; i < n; i++) {
-            if (str.charAt(i) != str.charAt(n-i-1)) {
-                return false;
-            }
-        }
-        return true;
-
-
-        // Alternative
-                // int start = 0, end = n-1;
+        // int start = 0, end = n-1;
         //   while (start <= end) {
         //     if (str.charAt(start) != str.charAt(end)) {
         //         return false;
@@ -22,6 +12,17 @@ public class CheckPlanidrome {
         //     end--;
         // }
         // return true;
+
+
+        // Alternative
+        int n = str.length();
+
+        for (int i = 0; i < n; i++) {
+            if (str.charAt(i) != str.charAt(n-i-1)) {
+                return false;
+            }
+        }
+        return true;
 
     }
     public static void main(String[] args) {
