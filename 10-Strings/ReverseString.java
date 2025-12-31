21967f6479cadb39340fee8522ad1dd72ae284a7
Muhammad Abdullah <mabdullah00023@gmail.com>
2025-12-31 22:02:17 +0500

    first commit

--- 10-Strings/ReverseString.java
+++ 10-Strings/ReverseString.java
@@ -2,19 +2,28 @@ public class ReverseString {
     
     public static String reverse(String str) {
       
-        StringBuilder sb = new StringBuilder(str);
+        // StringBuilder sb = new StringBuilder(str);
 
-        int start = 0, end = sb.length()-1;
-        while (start < end) {
+        // int start = 0, end = sb.length()-1;
+        // while (start < end) {
             
-            char temp = sb.charAt(start);
-            sb.setCharAt(start, sb.charAt(end));
-            sb.setCharAt(end, temp);
-            start++;
-            end--;
+        //     char temp = sb.charAt(start);
+        //     sb.setCharAt(start, sb.charAt(end));
+        //     sb.setCharAt(end, temp);
+        //     start++;
+        //     end--;
+        // }
+        
+        // return sb.toString();
+
+        // Another Way
+        String reversed = "";
+
+        for (int i = str.length()-1; i >= 0; i--) {
+            reversed = reversed  + str.charAt(i);
         }
         
-        return sb.toString();
+        return reversed;
     }
     public static void main(String[] args) {
         String str = "TAB";
