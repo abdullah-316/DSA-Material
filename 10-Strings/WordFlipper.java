e1734ea83bd511912f1859989365cbf9eb2bde71
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-02 17:50:30 +0500

    first commit

---
+++ 10-Strings/WordFlipper.java
@@ -0,0 +1,18 @@
+public class WordFlipper {
+    
+    public static String flipper(String sentence) {
+        String words[] = sentence.split(" ");
+        StringBuilder sb = new StringBuilder("");
+
+        for (int i = words.length-1; i >= 0; i--) {
+            String word = words[i];
+            StringBuilder revWords = new StringBuilder(word);
+            sb.append(revWords.reverse()).append(" ");
+        }
+        return sb.toString().trim();
+    }
+    public static void main(String[] args) {
+        String sentence = "Coding is Fun";
+        System.out.println(flipper(sentence));
+    }
+}
