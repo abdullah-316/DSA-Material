84d51654c5a1885ec494685ae0d43fe69eeb2cd2
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-06 19:42:35 +0500

    first commit

---
+++ 6-Arrays/Duplicate.java
@@ -0,0 +1,24 @@
+public class Duplicate {
+
+    public static int findDuplicate(int arr[]) {
+        int n = arr.length;
+        boolean[] check = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+
+            if (check[num]) {
+                return num;
+            }
+            check[num] = true;
+        }
+        return -1;
+    }
+
+    
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 1, 1, 2, 2 };
+        System.out.println(findDuplicate(arr));
+    }
+}
