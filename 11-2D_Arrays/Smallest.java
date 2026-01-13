4d0f7228e940179c6f3fdeb0458c0c4a34ade08b
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-13 23:03:32 +0500

    first commit

---
+++ 11-2D_Arrays/Smallest.java
@@ -0,0 +1,31 @@
+public class Smallest {
+    
+    public static void smallest(int matrix[][]) {
+        
+        int small = Integer.MAX_VALUE;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (small > matrix[i][j]) {
+                    small = matrix[i][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (small == matrix[i][j]) {
+                    System.out.println("smallest cell (" + i + "," + j + ")");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = {{2,4,8},
+                          {3,6,9},     
+                          {1,0,7}};
+
+    smallest(matrix);
+    }
+}
