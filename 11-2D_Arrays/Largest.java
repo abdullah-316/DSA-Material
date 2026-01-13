dc12d39b7453d5d218b81e3d302ae70ab758d2fa
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-13 23:02:34 +0500

    first commit

---
+++ 11-2D_Arrays/Largest.java
@@ -0,0 +1,36 @@
+public class Largest {
+    
+    public static int largest(int matrix[][]) {
+        int large = Integer.MIN_VALUE;
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (large < matrix[i][j]) {
+                    large = matrix[i][j];
+                }
+            }
+        }
+        return large;
+    }
+
+    public static void print(int mat[][], int largeVal) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (largeVal == mat[i][j]) {
+                    
+                    System.out.print("largest cell (" + i + "," + j + ")");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{2,4,8},
+                          {3,6,9},     
+                          {1,5,7}};
+        int largeVal = largest(matrix);
+        print(matrix, largeVal);
+    }
+}
