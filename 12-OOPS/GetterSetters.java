9a7eeb292f28b8d8d7a5e33e7b01139f17e3051a
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-16 10:55:07 +0500

    first commit

--- 12-OOPS/GetterSetters.java
+++ 12-OOPS/GetterSetters.java
@@ -22,4 +22,57 @@ class Person {
             this.password = pwd;
         }
     
+class Student {
+        private String name;
+        private int age;
+    
+        // Getter for name
+        public String getName() {
+            return name;
+        }
+    
+        // Setter for name
+        public void setName(String n) {
+            name = n;
+        }
+    
+        // Getter for age
+        public int getAge() {
+            return age;
+        }
+    
+        // Setter for age with condition
+        public void setAge(int a) {
+            if(a > 0) {
+                age = a;
+            }
+        }
+    }
+
+
+    /*
+     * Defination:
+                  "Getters and setters are used to access and update private variables of a class. They support encapsulation and allow us to add logic, such as validation, before changing values."
+
+     * Diagram Style (Text-Based):
+     
+    Class: Student
+     ---------------------
+     | private name       |
+     | private age        |
+     ---------------------
+     | getName()          |
+     | setName(String n)  |
+     | getAge()           |
+     | setAge(int a)      |
+     ---------------------
+
+     Access:
+     s.setName("Ali");   ✅
+     s.getName();        ✅
+     s.name = "Ali";     ❌ (Not allowed — it's private)
+     
+     */
+        
 }
+
